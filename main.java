2ac171a3ddf5785a0eeb826ce00b790204dc62eb
Dominik Velic <xvelic@stuba.sk>
2023-12-18 19:52:54 +0100

    implemented basic game loop + images

--- main.java
+++ main.java
@@ -1,1 +1,45 @@
 console.log("loaded");
+
+let rocket = document.getElementById("rocket");
+
+let fps = 60;
+let frameTime = 1000 / fps; // Time for each frame in milliseconds
+let lastTime = performance.now();
+
+let lastTime = 0;
+
+function gameLoop(currentTime) {
+    let deltaTime = currentTime - lastTime;
+
+    if (deltaTime < frameTime) {
+        // If not enough time has passed for the next frame, wait
+        requestAnimationFrame(gameLoop);
+        return;
+    }
+
+    lastTime = currentTime;
+
+    // Your game logic goes here
+
+    requestAnimationFrame(gameLoop);
+}
+
+
+function startGame(){
+
+    menuScreen();
+    // Start the game loop
+    requestAnimationFrame(gameLoop);
+    
+}
+
+
+function menuScreen(){
+
+    while(true){
+
+    }
+
+}
+
+startGame();
