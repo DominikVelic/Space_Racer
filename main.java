6b572d25e093283ad6e0a2f3dde205ca47c2ebe8
Dominik Velic <xvelic@stuba.sk>
2023-12-20 22:05:55 +0100

    drz picu Erik

--- main.java
+++ main.java
@@ -2,41 +2,52 @@ console.log("loaded");
 
 function startGame() {
     myGameArea.start();
-    myGamePiece = new component(30, 30, "rocket", 10, 120);
+    myGamePiece = new component("rocket",500,400,80,120);
 }
-
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 1000;
+        this.canvas.height = 800;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 50);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
 var myGamePiece;
 
-function component(width, height, objectType, x, y) {
+function component(objectType, x, y, width, height) {
   this.width = width;
   this.height = height;
   this.x = x;
   this.y = y;
   ctx = myGameArea.context;
-  let img = new Image();
-  img.src = findObjectSrc(objectType);
-  image.onload = function() {
-    ctx.drawImage(image, x, y, width, height);
-  };
+
+  this.update = function(){
+      let img = new Image();
+      img.src = findObjectSrc(objectType);
+      img.onload = function() {
+        ctx.drawImage(img, x, y, width, height);
+      };
+  }
+}
+
+function updateGameArea(){
+    myGameArea.clear();
+    myGamePiece.update();
 }
 
 function findObjectSrc(objectType){
     switch(objectType){
         case 'rocket':
-            return '/img/rocket.png';
+            return './img/rocket.png';
             break;
         case 'meteor':
-            return '/img/meteor.png';
-            return;
+            return './img/meteor.png';
+            break;
     }
 }
