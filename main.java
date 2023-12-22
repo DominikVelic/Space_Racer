7c6efba3ceabeccde2b05996d3c3c1aa19db1fa4
Erik Vladar <erikvladar47@gmail.com>
2023-12-22 23:56:12 +0100

    čierne okolie

--- main.java
+++ main.java
@@ -131,7 +131,7 @@ function updateGameArea(){
     RocketPlayer.resetSpeed();
     GameArea.pressedKey();
     RocketPlayer.move();
-    RocketPlayer.update();    
+    RocketPlayer.update();   
 }
 
 function findObjectSrc(objectType){
